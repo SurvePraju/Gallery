25f0203f5ae5166809a1948ca32e2176d40f5902
Prajwal Surve <survepraju99@gmail.com>
2023-08-13 19:16:24 +0530

    fuctions in urls

--- basicapp/urls.py
+++ basicapp/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path, re_path
-from . import views
+from .views import *
+
+
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="user_login"),
-    path("upload-image/", views.upload_image, name="upload_image"),
+    path("", home, name="home"),
+    path("register/", register, name="register"),
+    path("login/",  login, name="user_login"),
+    path("upload-image/",  upload_image, name="upload_image"),
     path("images/<str:category>/",
-         views.display_categories, name="image_category"),
-    path("logout/", views.user_logout, name="logout"),
-    path("logged-in-user-profile/", views.profile, name="profile"),
-    path("image/<int:id>", views.view_image, name="view_images"),
-    path("try/", views.trail, name="try"),
-    path("user-id-<int:id>/", views.select_user, name="selected_user_images"),
+         display_categories, name="image_category"),
+    path("logout/",  user_logout, name="logout"),
+    path("logged-in-user-profile/",  profile, name="profile"),
+    path("image/<int:id>",  view_image, name="view_images"),
+    path("try/",  trail, name="try"),
+    path("user-id-<int:id>/",  select_user, name="selected_user_images"),
     path("update-user-profile/<int:id>/",
-         views.update_user, name="update_user"),
-    path("delete-image/<int:id>/", views.delete_image, name="delete_image"),
-    path("edit-image/<int:id>/", views.edit_image, name="edit_image"),
+         update_user, name="update_user"),
+    path("delete-image/<int:id>/",  delete_image, name="delete_image"),
+    path("edit-image/<int:id>/",  edit_image, name="edit_image"),
 
 ]
